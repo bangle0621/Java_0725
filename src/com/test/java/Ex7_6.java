25ae4b72673940c7ef1ed0aa899bc34aa17e0c70
bangle <lmh3294@naver.com>
2022-12-23 17:22:16 +0900

    static import

---
+++ src/com/test/java/Ex7_6.java
@@ -0,0 +1,12 @@
+package com.test.java;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class Ex7_6 {
+    public static void main(String args[]){
+        System.out.println();
+        out.println("static Math PI : "+ PI);
+
+    }
+}
