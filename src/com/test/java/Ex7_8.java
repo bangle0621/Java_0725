af301368f5cc74caa589f5ad30f032846c980bd0
bangle <lmh3294@naver.com>
2023-01-03 22:50:12 +0900

    매개변수의 다형성

--- src/com/test/java/Ex7_8.java
+++ src/com/test/java/Ex7_8.java
@@ -7,9 +7,11 @@ public class Ex7_8 {
 
         b.buy(new Tv());
         b.buy(new Computer());
+        b.summary();
 
         System.out.println("현재 남은 돈은 "+ b.money+" 입니다.");
         System.out.println("현재 남은 보너스 포인트는 " + b.bonusPoint + " 입니다.");
+
 
     }
 }
@@ -43,7 +45,8 @@ class Computer extends Product{
 class Buyer{
     int money = 1000;
     int bonusPoint = 0;
-
+    int i = 0;
+    Product[] cart = new Product[10];
     void buy(Product p){
         if(money < p.price){
             System.out.println("돈이 부족하여 물건을 살 수 없습니다.");
@@ -52,5 +55,20 @@ class Buyer{
 
         money -= p.price;
         bonusPoint += p.bonusPoint;
+        cart[i] = p;
+        i++;
+    }
+
+    void summary(){
+        int sum = 0;
+        String itemList = "";
+
+        for(int i=0; i< cart.length; i++){
+            if(cart[i] == null) break;
+            sum += cart[i].price;
+            itemList += cart[i].toString() + ", ";
+        }
+        System.out.println("구입하신 금액은 "+ sum +"입니다.");
+        System.out.println("구입하신 목록은 " + itemList + "입니다.");
     }
 }
